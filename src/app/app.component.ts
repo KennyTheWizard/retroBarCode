8502d782cc5514e29d75543ed5a0fc0c0ec2830e
KennyTheWizard <kenngineering@gmail.com>
2017-11-08 20:31:18 -0600

    initial commit with barcode working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  colorSelection: string[] = [
+    'red',
+    'yellow',
+    'green',
+    'blue',
+    'grey',
+    'salmon',
+    'brown',
+  ]
+  colors: string[];
+  getColor(){
+    let retArr: string[] = [];
+    for (var i = 0; i < 15; i++) {
+      retArr.push(
+        this.colorSelection[Math.floor(Math.random()*this.colorSelection.length)]
+      ); 
+    }
+    return retArr;
+  }
+
+  ngOnInit() {
+    this.colors = this.getColor();
+    console.log(this.colors);
+  }
 }
